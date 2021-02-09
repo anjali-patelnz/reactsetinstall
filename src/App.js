3e08c3b96a421e6821ba7362d6472b8dd512cd87
anjali-patelnz <73802454+anjali-patelnz@users.noreply.github.com>
2021-02-09 15:24:02 +1300

    Changed the loader colour to pink

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App() {
       <header className="App-header">
         <Loader
           type="Puff"
-          color="blue"
+          color="pink"
           height={100}
           width={100}
           timeout={3000} //3 secs
